2379abd874160aaed761078e5e49c7208ac8cba8
kazusystemakure <70381817+kazusystemakure@users.noreply.github.com>
2020-09-26 17:36:20 +0900

    レビュー修正6 単体テスト追記

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -6,78 +6,81 @@ RSpec.describe Item, type: :model do
   end
 
   describe '商品出品テスト' do
-    context 'データ保存成功'
+    context 'データ保存成功' do
 
-    it '全ての項目が存在すれば登録できること' do
-      expect(@item).to be_valid
+      it '全ての項目が存在すれば登録できること' do
+        expect(@item).to be_valid
+      end
     end
 
-    context 'データ保存失敗'
+    context 'データ保存失敗' do
 
-    it 'imageが空では登録できないこと' do
-      @item.image = nil
-      @item.valid?
-      expect(@item.errors.full_messages).to include("Image can't be blank")
-    end
+      it 'imageが空では登録できないこと' do
+        @item.image = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Image can't be blank")
+      end
 
-    it 'nameが空では登録できないこと' do
-      @item.name = nil
-      @item.valid?
-      expect(@item.errors.full_messages).to include("Name can't be blank")
-    end
+      it 'nameが空では登録できないこと' do
+        @item.name = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Name can't be blank")
+      end
 
-    it 'textが空では登録できないこと' do
-      @item.text = nil
-      @item.valid?
-      expect(@item.errors.full_messages).to include("Text can't be blank")
-    end
+      it 'textが空では登録できないこと' do
+        @item.text = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Text can't be blank")
+      end
 
-    it 'categoryが空では登録できない' do
-      @item.category_id = 1
-      @item.valid?
-      expect(@item.errors.full_messages).to include('Category Select')
-    end
+      it 'categoryが空では登録できない' do
+        @item.category_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Category Select')
+      end
 
-    it 'statusが空では登録できない' do
-      @item.status_id = 1
-      @item.valid?
-      expect(@item.errors.full_messages).to include('Status Select')
-    end
+      it 'statusが空では登録できない' do
+        @item.status_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Status Select')
+      end
 
-    it 'shipping_feeが空では登録できない' do
-      @item.shipping_fee_id = 1
-      @item.valid?
-      expect(@item.errors.full_messages).to include('Shipping fee Select')
-    end
+      it 'shipping_feeが空では登録できない' do
+        @item.shipping_fee_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Shipping fee Select')
+      end
 
-    it 'prefectureが空では登録できない' do
-      @item.prefecture_id = 1
-      @item.valid?
-      expect(@item.errors.full_messages).to include('Prefecture Select')
-    end
+      it 'prefectureが空では登録できない' do
+        @item.prefecture_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Prefecture Select')
+      end
 
-    it 'scheduled_deliveryが空では登録できない' do
-      @item.scheduled_delivery_id = 1
-      @item.valid?
-      expect(@item.errors.full_messages).to include('Scheduled delivery Select')
-    end
+      it 'scheduled_deliveryが空では登録できない' do
+        @item.scheduled_delivery_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Scheduled delivery Select')
+      end
 
-    it 'priceが空では登録できないこと' do
-      @item.price = nil
-      @item.valid?
-      expect(@item.errors.full_messages).to include("Price can't be blank")
-    end
+      it 'priceが空では登録できないこと' do
+        @item.price = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price can't be blank")
+      end
 
-    it 'priceが300円未満で登録できないこと' do
-      @item.price = 299
-      @item.valid?
-      expect(@item.errors.full_messages).to include('Price Out of setting range')
-    end
+      it 'priceが300円未満で登録できないこと' do
+        @item.price = 299
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Price Out of setting range')
+      end
 
-    it 'priceが9999999円以上は登録できないこと' do
-      @item.price = 10_000_000
-      @item.valid?
-      expect(@item.errors.full_messages).to include('Price Out of setting range')
+      it 'priceが9999999円以上は登録できないこと' do
+        @item.price = 10_000_000
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Price Out of setting range')
+      end
+
     end
   end
 end
